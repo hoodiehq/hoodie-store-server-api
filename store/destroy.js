9fc0ff86150333d6722de5fb8fbb9aa27e2735a6
Gregor <gregor@martynus.net>
2017-03-04 23:47:54 -0800

    feat: database access cache

--- store/destroy.js
+++ store/destroy.js
@@ -3,7 +3,7 @@ module.exports = destroyStore
 var getDocOrFalse = require('../utils/get-doc-or-false')
 
 function destroyStore (state, name) {
-  return getDocOrFalse(state.stateStore, name)
+  return getDocOrFalse(state, name)
 
   .then(function (doc) {
     if (doc === false) {
@@ -14,7 +14,8 @@ function destroyStore (state, name) {
     return state.stateStore.update(doc)
   })
 
-  .then(function () {
+  .then(function (doc) {
+    state.cache.unset(doc._id)
     return new state.PouchDB(name).destroy()
   })
 
